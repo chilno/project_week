a4be5c0a307c0e66b33d9dab292ab133fc7b02a2
Ana Thomas <anathomasity@gmail.com>
2016-06-28 16:27:38 -0700

    'seeds'

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+items = Item.create([{name: 'Umbrella', price: 5},{name: 'Phone', price: 100},{name: 'Watch', price: 20},{name: 'Bottle', price: 5},{name: 'Car', price: 50},{name: 'TV', price: 40},{name: 'DVD', price: 20}])
