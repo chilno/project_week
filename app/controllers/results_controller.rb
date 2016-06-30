8a12ff2b01be731be274c80f31960d00b5f691ee
Nazim Bellahsene <nazim.bellahsene@gmail.com>
2016-06-30 13:55:20 -0700

    category search

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -7,9 +7,14 @@ class ResultsController < ApplicationController
   end
 
   def create
-  	terms = params[:search].split(" ")
-  	query = terms.map {|term| "name LIKE '%#{term}%'"}.join(" OR ")
-  	@search = Item.where(query)
+  	if params[:search]
+  		terms = params[:search].split(" ")
+  		query = terms.map {|term| "name LIKE '%#{term}%'"}.join(" OR ")
+  		@search = Item.where(query)
+  	else
+  		@search = Item.where(category_id: params[:catName])
+
+  	end
 
   end
   	# @search = User.joins(:items).select('items.id','state').where(state: current_user.state)
